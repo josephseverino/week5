a98d19a60111f3b9f200c3ec7f95f8788910a1fc
Joseph Severino <joseph.severino@colorado.edu>
2016-08-02 17:48:01 +0000

    first commit

---
+++ magellan/server.js
@@ -0,0 +1,48 @@
+var express     = require('express');
+var bodyParser   = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname +'/public'))
+
+app.get('/', function(req, res){
+    console.log('Homepage is rendered');
+    res.sendFile('index.html', {root: './public/html'})
+})
+
+app.get('/canary', function(req, res){
+ 
+    res.sendFile('canary.html', {root: './public/html'})
+})
+
+app.get('/cape', function(req, res){
+    
+    res.sendFile('cape.html', {root: './public/html'})
+})
+
+app.get('/strait', function(req, res){
+   
+    res.sendFile('strait.html', {root: './public/html'})
+})
+
+app.get('/guam', function(req, res){
+   
+    res.sendFile('guam.html', {root: './public/html'})
+})
+
+app.get('/phillipines', function(req, res){
+   
+    res.sendFile('phillipines.html', {root: './public/html'})
+})
+
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json())
+
+app.post('/nextlocation', function(req, res){
+    
+})
+
+app.listen( process.env.PORT || 3000, function(){
+    console.log('Server is running');
+})
